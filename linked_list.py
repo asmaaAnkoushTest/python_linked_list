4a63f9a0c11b31193a1debe9c3600f5194b97b9b
asmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-20 06:03:04 +0200

    feat: implement append method to append elements to begning of the list

--- linked_list.py
+++ linked_list.py
@@ -9,6 +9,15 @@ class LinkedList:
         self.head = None
         self.length = 0
     
+    def append(self, value):
+        node: Node = Node(value)
+        if self.head is None:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+        self.length += 1
+
     def add(self, value):
         # Edge case
         node: Node = Node(value)
@@ -21,17 +30,6 @@ class LinkedList:
             current.next = node
         self.length += 1
 
-    def append(self, value):
-        node: Node = Node(value)
-        if self.head is None:
-            self.head = node
-        else:
-            node.next = self.head.next
-            self.head.next = node
-        self.length += 1
-
-
-
     def print_list(self):
         current: Node = self.head
         while current:
@@ -40,9 +38,10 @@ class LinkedList:
 
 
     
-# print("##HIII##")
-ll = LinkedList()
-ll.append(10)
+# ll = LinkedList()
+# ll.append(10)
+# ll.append(5)
+# ll.add(20)
 
-print(ll.length)
-ll.print_list()
+# print(ll.length)
+# ll.print_list()
