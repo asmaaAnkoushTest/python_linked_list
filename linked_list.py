deabbf263a123786306f599d70e5fa55822e7625
asmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-20 05:41:06 +0200

    feat: implement add method to append elements to the list

--- linked_list.py
+++ linked_list.py
@@ -10,3 +10,30 @@ class LinkedList:
         self.head = None
         self.length = 0
     
+    def add(self, value):
+        # Edge case
+        node: Node = Node(value)
+        if self.head is None:
+            self.head = node 
+        else :
+            current: Node = self.head
+            while current.next:
+                current = current.next
+            current.next = node
+        self.length += 1
+
+
+    def print_list(self):
+        current: Node = self.head
+        while current:
+            print(f"{current.value} -> ", end = "")
+            current = current.next
+
+
+    
+# print("##HIII##")
+# ll = LinkedList()
+# ll.add(10)
+
+# print(ll.length)
+# ll.print_list()
