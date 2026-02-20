929d4da4287c1e0926956edf63bb619ee4c2f730
asmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-20 05:39:33 +0200

    test: add insert test that insert to the end of linked list

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -9,3 +9,11 @@ def test_linked_test_creation():
     ll:LinkedList = LinkedList()
     assert ll.head is None
     assert ll.length == 0
+
+def test_add_and_length():
+    ll = LinkedList()
+    ll.add(10)
+    ll.add(20)
+    ll.add(30)
+
+    assert ll.length == 3
